2b1bce3f27cb95cff4da19c243802ff7156233c9
William Wagner <wcwwagner@gmail.com>
2017-12-04 21:31:24 -0500

    Update Submission mapping types

--- core/index_submissions_queue.py
+++ core/index_submissions_queue.py
@@ -3,11 +3,9 @@ Takes the Reddit redis submission queue and indexes them into the ElasticSearch 
 Before indexing, each submission is associated with a sentiment score, via `link_to_sentiment.py'.
 """
 #!/usr/bin/env python
-import datetime
 import elasticsearch as es
 import json
 import praw
-import sys
 import time
 from datetime import datetime, timedelta
 from utils.redisqueue import RedisQueue
@@ -25,7 +23,7 @@ def create_submissions_index(client):
                     'format': 'epoch_second',
                 },
                 'domain': {
-                    'type': 'integer',
+                    'type': 'string',
                 },
                 'downs': {
                     'type': 'integer',
@@ -40,10 +38,10 @@ def create_submissions_index(client):
                     'type': 'integer',
                 },
                 'selftext': {
-                    'type': 'string',
+                    'type': 'text',
                 },
                 'sentiment': {
-                    'type': 'integer',
+                    'type': 'float',
                 },
                 'subreddit': {
                     'type': 'string',
@@ -70,6 +68,7 @@ def create_submissions_index(client):
     body = {'mappings': submission_mapping}
 
     try:
+        print('Creating submission index')
         es_client.indices.create(index='reddit', body=body)
     except es.exceptions.TransportError as e:
         if e.error != 'index_already_exists_exception':
@@ -79,9 +78,10 @@ def create_submissions_index(client):
 def index_submission(es_client, submission):
     try:
         print(f'Indexing {submission["score"]}--{submission["title"]}')
-        es_client.index(index='reddit', doc_type='submission', id=submission['id'], body=submission )
+        submission['created_utc'] = int(submission['created_utc'])
+        es_client.index(index='reddit', doc_type='submission', id=submission['id'], body=submission)
     except (ValueError, IndexError, es.ElasticsearchException) as e:
-        print("Error indexing comment, skipping")
+        print(e)
         pass
 
 
@@ -91,18 +91,20 @@ def main(es_client, queue):
     ONE_DAY = timedelta(1)
     while True:
         if queue.qsize() == 0:
+            print(f'Queue empty {datetime.utcnow()}')
             time.sleep(1)  # avoid excessive checks
             continue
         submission = json.loads(queue.front())
         submission_created_dt = datetime.fromtimestamp(submission['created_utc'])
         time_diff = datetime.utcnow() - submission_created_dt  # TimeDelta of time elapsed between submission and now
-        if time_diff < ONE_DAY:
+        if False: #time_diff < ONE_DAY:
             print(f'Sleeping until {submission_created_dt + ONE_DAY}')
             time.sleep(int(time_diff.total_seconds() + 1))  # sleep until the front of the queue becomes a day old
             continue
         # retrieve sentiment score and index into ElasticSearch
         submission['sentiment'] = ss.get_sentiment(f'https://reddit.com/{submission["id"]}')
         index_submission(es_client, submission)
+        queue.get()  # remove submission from queue, as it has been indexed
 
 
 if __name__ == '__main__':
@@ -114,8 +116,8 @@ if __name__ == '__main__':
         username=REDDIT_CREDENTIALS['username'],
         password=REDDIT_CREDENTIALS['password'])
     queue = RedisQueue('reddit-submissions-queue')
-    create_submissions_index(es_client)
+    if not es_client.indices.exists(index="reddit"):
+        create_submissions_index(es_client)
     main(es_client, queue)
-
 
 
