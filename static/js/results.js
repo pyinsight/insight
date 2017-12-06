c061f5c991e70958fecb74c70cbea10a630d777e
William Wagner <wcwwagner@gmail.com>
2017-12-06 11:26:04 -0500

    Implement subreddit filter event handlers

--- static/js/results.js
+++ static/js/results.js
@@ -1,24 +1,56 @@
+function hideAllHits(hits) {
+    console.log('Hiding all hits');
+    hits.each(function(idx, div){
+        $(div).hide();
+    });
+}
+
+function showAllHits(hits) {
+    console.log('Showing all hits');
+    hits.each(function(idx, div){
+        $(div).show();
+    });
+}
+
+function uncheckAllBoxes(){
+    console.log("Deselecting all checkboxes");
+    var checkboxes = $('#subredditSubmenu').find('input');
+    checkboxes.each(function(idx, input){
+        if ($(input).prop('id') !== 'all')
+            $(input).prop('checked', false);
+    });
+}
+
+function showHitsBySubreddit(subreddit){
+    $(`#hits>div[class*=${subreddit}]`).show();
+}
+
+function hideHitsBySubreddit(subreddit){
+    $(`#hits>div[class*=${subreddit}]`).hide();
+}
+
 $(function() {
     $(".subreddit-checkbox").on("click", function(e){
-        console.log(e);
-        console.log(e.target.id)
+        var hits = $('#hits').find('div');
         if (e.target.id == 'all'){
-            console.log("Deselecting all checkboxes");
+            uncheckAllBoxes();
             if (e.target.checked){
-                console.log('Showing all hits');
+                showAllHits(hits);
             }
             else {
-                console.log('Hiding all hits');
+                hideAllHits(hits);
             }
         }
         else {
-            console.log("Deselecting r/all")
             if (e.target.checked){
-                console.log('Filtering posts')
+                if ($('#all').prop('checked')){
+                    $('#all').prop('checked', false);
+                    hideAllHits(hits);
+                }
+                showHitsBySubreddit(e.target.id);
             }
             else {
-                console.log('Hiding posts')
-            }
+                hideHitsBySubreddit(e.target.id);            }
         }
   })
 });
