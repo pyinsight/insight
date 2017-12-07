0f6c3f12c8b64c3ee91edfe2c4579e54d887f240
William Wagner <wcwwagner@gmail.com>
2017-12-07 11:05:14 -0500

    Handle invalid historical submissions

--- core/index_historical_submissions.py
+++ core/index_historical_submissions.py
@@ -3,22 +3,16 @@ import elasticsearch as es
 import csv
 import sys
 import os
+import time
+import requests
 from glob import glob
-from link_to_sentiment import SubmissionSentimizer
+from submission_ext import SubmissionExt
 from utils.elasticsearch_utils import create_submissions_index
 
 
-FIELDS_TO_KEEP = [
+FIELDS_TO_INDEX = [
     'author', 'created_utc', 'domain', 'downs', 'id', 'num_comments',
-    'score', 'selftext', 'subreddit', 'subreddit_id', 'thumbnail', 'title', 'ups', 'url'
-]
-
-NON_NULLABLE_FIELDS = [
-    'author', 'created_utc', 'id', 'subreddit', 'title', 'url'
-]
-
-NUMERIC_FIELDS = [
-    'created_utc', 'downs', 'num_comments', 'score', 'ups'
+    'score', 'selftext', 'sentiment', 'subreddit', 'subreddit_id', 'thumbnail', 'title', 'ups', 'url'
 ]
 
 
@@ -38,17 +32,17 @@ def create_arg_parser():
     return parser
 
 
-def index_submission(es_client, submission_doc):
+def index_submission(es_client, submission_dict):
     """ Indexes the submission into the elasticsearch reddit index """
     try:
-        print(f'Indexing {submission_doc["score"]}--{submission_doc["title"]}')
-        es_client.index(index='reddit', doc_type='submission', id=submission_doc['id'], body=submission_doc)
+        print(f'Indexing {submission_dict["score"]}--{submission_dict["title"]}')
+        es_client.index(index='reddit', doc_type='submission', id=submission_dict['id'], body=submission_dict)
     except (ValueError, IndexError, es.ElasticsearchException) as e:
         print(e)
         pass
 
 
-def index_submissions_csv_file(csvfile, es_client, sub_sentimizer):
+def index_submissions_csv_file(csvfile, es_client):
     """
     Indexes the `csvfile` by prearing each line to a document compatible with the elasticsearch submission schema.
     """
@@ -56,14 +50,13 @@ def index_submissions_csv_file(csvfile, es_client, sub_sentimizer):
     # read in header and map property names to their respective indexes
     index_of = {prop: index for index, prop in enumerate(next(csvreader))}
     for i, line in enumerate(csvreader):
-        doc = {field: line[index_of[field]] for field in FIELDS_TO_KEEP}
-        if not all((doc[field] for field in NON_NULLABLE_FIELDS)):
-            # print('Skipping doc: {doc}', file=sys.stderr)
+        time.sleep(5)
+        try:
+            submission_id = line[index_of['id']]
+            submission = SubmissionExt(id=submission_id)
+            index_submission(es_client, submission.to_dict(FIELDS_TO_INDEX))
+        except (requests.exceptions.RequestException, ValueError):
             continue
-        for field in NUMERIC_FIELDS:
-            doc[field] = coerce_to_int(doc[field])
-        doc['sentiment'] = sub_sentimizer.get_sentiment(f'https://reddit.com/{doc["id"]}')
-        index_submission(es_client, doc)
 
 
 def main():
@@ -76,7 +69,6 @@ def main():
         sys.exit(1)
 
     es_client = es.Elasticsearch()
-    sub_sentimizer = SubmissionSentimizer()  # expensive to setup, so only do once
     if not es_client.indices.exists(index="reddit"):
         create_submissions_index(es_client)
 
@@ -84,7 +76,7 @@ def main():
     # glob finds all pathnames matching specified datapath_pattern
     for csv_path in glob(datapath_pattern):
         with open(csv_path) as csvfile:
-            index_submissions_csv_file(csvfile, es_client, sub_sentimizer)
+            index_submissions_csv_file(csvfile, es_client)
 
 
 if __name__ == '__main__':
--- core/submission_ext.py
+++ core/submission_ext.py
@@ -3,6 +3,19 @@ from secrets import REDDIT_CREDENTIALS
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 
 
+NON_NULLABLE_FIELDS = [
+    'author', 'created_utc', 'id', 'subreddit', 'title', 'url'
+]
+
+def is_valid_submission(submission):
+    _ = submission.title
+    fields_dict = vars(submission)
+    for field in NON_NULLABLE_FIELDS:
+        if field not in fields_dict or fields_dict[field] in ['', None]:
+            return False
+    return True
+
+
 class SubmissionExt(praw.models.Submission):
     """
     Small child class of praw.models.Submission that provides a sentiment score attribute
@@ -21,6 +34,9 @@ class SubmissionExt(praw.models.Submission):
 
     def __init__(self, url=None, id=None):
         super().__init__(SubmissionExt.reddit, url=url, id=id)  # will raise if id and url are None
+        if not is_valid_submission(self):
+            print(f'https://reddit.com/{self.id}')
+            raise ValueError("Submission is broken")
         self.sentiment = self._calculate_sentiment()
 
     def __repr__(self):
@@ -36,8 +52,11 @@ class SubmissionExt(praw.models.Submission):
         """
         fields_dict = vars(self)
         # Coerce complex fields to their string representations
-        fields_dict['subreddit'] = fields_dict['subreddit'].display_name
-        fields_dict['author'] = fields_dict['author'].name if fields_dict['author'] else 'deleted'
+        if not isinstance(fields_dict['subreddit'], str):
+            fields_dict['subreddit'] = fields_dict['subreddit'].display_name
+        if not isinstance(fields_dict['author'], str):
+            fields_dict['author'] = fields_dict['author'].name if fields_dict['author'] else 'deleted'
+        fields_dict['created_utc'] = int(fields_dict['created_utc'])
         # filter down submission to just the fields in elasticsearch schema
         fields_dict_filtered = {field: fields_dict[field] for field in fields_to_keep}
         return fields_dict_filtered
